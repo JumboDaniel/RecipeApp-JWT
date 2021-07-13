143479e9c4c6e803195d3f30294f888841dfd1de
Jumbo Daniel <jumbodaniel9@gmail.com>
2021-07-13 23:26:50 +0800

    Created the User model for signup post request

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,5 @@
+const User = require('../model/user');
+
 module.exports.signup_get = (req, res)=>{
     res.render('signup')
 }
@@ -6,14 +8,19 @@ module.exports.login_get = (req, res)=>{
     res.render('signup')
 }
 
-module.exports.signup_post = (req, res)=>{
-    const {name, email} = req.body;
-    console.log(name , email)
+module.exports.signup_post = async (req, res)=>{
+    const {name, email, password} = req.body;
+    try {
+        const user = await User.create({name, email, password})
+        res.status(201).json(user)
+    } catch (err) {
+        console.log(err)
+        res.status(400).send('User not created')
+    }
     res.send('new signup')
 }
 
 module.exports.login_post = (req, res)=>{
     const {name, email} = req.body;
-    console.log(name , email)
     res.send('new login')
 }
--- model/user.js
+++ model/user.js
@@ -1,3 +1,22 @@
 const mongoose = require('mongoose');
-const Schema =mongoose.Schema();
+const Schema =mongoose.Schema;
 
+const userSchema = new Schema ({
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },  
+    password:{
+        type: String,
+        required: true,
+        minLength: 6
+    }
+})
+
+module.exports = mongoose.model('user', userSchema)
